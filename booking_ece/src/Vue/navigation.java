fbcb058efc3706f377fd6570b5313aa70fb35e2c
Aupiet <audric.piet@gmail.com>
2025-04-01 17:54:16 +0200

    V1.2 Ajout de la structure de base des page et de la navigation part 2

--- booking_ece/src/Vue/navigation.java
+++ booking_ece/src/Vue/navigation.java
@@ -3,10 +3,10 @@ package Vue;
 import javax.swing.*;
 import java.awt.*;
 
-
+// classe navigation qui gère la navigation entre les différentes pages de l'application
 public class navigation {
-    private CardLayout cardLayout;
-    private JPanel mainPanel;
+    private CardLayout cardLayout; // gestionnaire de mise en page
+    private JPanel mainPanel; // panneau principal qui contiendra toutes les pages
     public navigation() {
         JFrame frame = new JFrame("Booking.ECE");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,9 +16,12 @@ public class navigation {
         frame.add(mainPanel);
         frame.setVisible(true);
     }
+
+    // méthode pour ajouter une page au panneau principal
     public void addpage(pageStruct page, String cardName) {
         mainPanel.add(page, cardName);
     }
+    // méthode pour supprimer une page du panneau principal (pas encore testé)
     public void removePage(String cardName) {
         Component[] components = mainPanel.getComponents();
         for (Component component : components) {
@@ -28,13 +31,12 @@ public class navigation {
             }
         }
     }
+    // méthode pour afficher une page en fonction de son nom
     public void showCard(String cardName) {
         cardLayout.show(mainPanel, cardName);
     }
+    // méthode pour revenir à la page précédente
     public void nextPage(pageStruct panel) {
         cardLayout.next(mainPanel);
-    }
-    public void setpage(CardLayout cardLayout) {
-        this.cardLayout = cardLayout;
     }
 }
--- booking_ece/src/Vue/pageStruct.java
+++ booking_ece/src/Vue/pageStruct.java
@@ -2,12 +2,19 @@ package Vue;
 
 import javax.swing.*;
 
+// classe mère de toutes les pages qui contient les méthodes de navigation
 public class pageStruct extends JPanel {
     navigation nav;
     public pageStruct( navigation nav) {
         this.nav = nav;
-    }
+    } // constructeur de la classe pageStruct
+
+    // méthode pour naviguer vers une autre page
     public void moveToPage(String pageName) throws PageNotFoundException {
         nav.showCard(pageName);
+    }
+    // méthode pour revenir à la page précédente
+    public void previousPage() {
+        nav.nextPage(this);
     }
 }
